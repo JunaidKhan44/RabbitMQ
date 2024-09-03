fe9af858daf01175b50f2870064dc150d5f8a65d
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-09-03 14:33:01 +0500

    Update RabbitMQConsumer.java

--- jk.rabbitmqueue/src/main/java/jk/rabbitmqueue/demo/consumer/RabbitMQConsumer.java
+++ jk.rabbitmqueue/src/main/java/jk/rabbitmqueue/demo/consumer/RabbitMQConsumer.java
@@ -5,6 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author Junaid.Khan
+ */
+
+
 @Slf4j
 @Service
 public class RabbitMQConsumer {
